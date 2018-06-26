d31fadd00dca2f16b3516606d345da0fa1254859
onestraw <hexiaowei91@163.com>
2018-06-26 14:28:13 +0800

    More controller APIs

--- balancer/controller.go
+++ balancer/controller.go
@@ -21,8 +21,12 @@ type Controller struct {
 func (c *Controller) Run(service *Service) {
 	r := mux.NewRouter()
 	r.Handle("/stats", &StatsHandler{service}).Methods("GET")
-	r.Handle("/vs/{name}/{action}", ModifyVirtualServerStatus(service)).Methods("POST")
 	r.Handle("/vs", AddVirtualServer(service)).Methods("POST")
+	r.Handle("/vs", ListAllVirtualServer(service)).Methods("GET")
+	r.Handle("/vs/{name}", ModifyVirtualServerStatus(service)).Methods("POST")
+	r.Handle("/vs/{name}", ListVirtualServer(service)).Methods("GET")
+	r.Handle("/vs/{name}/pool", AddPoolMember(service)).Methods("POST")
+	r.Handle("/vs/{name}/pool", DeletePoolMember(service)).Methods("DELETE")
 	panic(http.ListenAndServe(c.Address, AuthMiddleware(c.Auth)(r)))
 }
 
@@ -41,18 +45,54 @@ func (h *StatsHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	io.WriteString(w, strings.Join(result, "\n"))
 }
 
+func ListAllVirtualServer(s *Service) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		for _, vs := range s.VServers {
+			data := fmt.Sprintf("Name:%s, Address:%s, Status:%s, Pool:\n%s\n\n",
+				vs.Name, vs.Address, vs.Status(), vs.Pool)
+			io.WriteString(w, data)
+		}
+	})
+}
+
+func ListVirtualServer(s *Service) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+		name := vars["name"]
+		vs, err := s.FindVirtualServer(name)
+		if err != nil {
+			log.Errorf("FindVirtualServ err=%v", err)
+			WriteBadRequest(w, err)
+			return
+		}
+		msg := vs.Pool.String()
+		io.WriteString(w, msg)
+	})
+}
+
+type ModifyVirtualServer struct {
+	Action string `json:"action"`
+}
+
 func ModifyVirtualServerStatus(s *Service) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		vars := mux.Vars(r)
 		name := vars["name"]
-		action := vars["action"]
+		var req ModifyVirtualServer
+		decoder := json.NewDecoder(r.Body)
+		if err := decoder.Decode(&req); err != nil {
+			log.Errorf("Decode request err=%v", err)
+			WriteBadRequest(w, err)
+			return
+		}
+		action := req.Action
 		log.Infof("virtual server name %s, action %s", name, action)
 		msg := "success"
 
 		vs, err := s.FindVirtualServer(name)
 		if err != nil {
 			log.Errorf("FindVirtualServ err=%v", err)
-			WriteError(w, ErrBadRequest)
+			WriteBadRequest(w, err)
 			return
 		}
 
@@ -83,7 +123,7 @@ func AddVirtualServer(s *Service) http.Handler {
 		err := decoder.Decode(&vs)
 		if err != nil {
 			log.Errorf("Decode request err=%v", err)
-			WriteError(w, ErrBadRequest)
+			WriteBadRequest(w, err)
 			return
 		}
 
@@ -91,11 +131,68 @@ func AddVirtualServer(s *Service) http.Handler {
 		err = s.AddVirtualServer(&vs)
 		if err != nil {
 			log.Errorf("AddVirtualServ err=%v", err)
-			resp := BalancerError{http.StatusBadRequest, err.Error()}
-			WriteError(w, resp)
+			WriteBadRequest(w, err)
 			return
 		}
 
 		io.WriteString(w, "Add success")
+	})
+}
+
+func decodeServer(r *http.Request) (*config.Server, error) {
+	var server config.Server
+	decoder := json.NewDecoder(r.Body)
+	err := decoder.Decode(&server)
+	if err != nil {
+		log.Errorf("Decode request err=%v", err)
+		return nil, err
+	}
+	return &server, nil
+}
+
+func AddPoolMember(s *Service) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+		name := vars["name"]
+		vs, err := s.FindVirtualServer(name)
+		if err != nil {
+			log.Errorf("FindVirtualServ err=%v", err)
+			WriteBadRequest(w, err)
+			return
+		}
+
+		server, err := decodeServer(r)
+		if err != nil {
+			WriteBadRequest(w, err)
+			return
+		}
+
+		weight := server.Weight
+		if weight <= 0 {
+			weight = 1
+		}
+		vs.AddPeer(server.Address, weight)
+		io.WriteString(w, "Add peer success")
+	})
+}
+
+func DeletePoolMember(s *Service) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+		name := vars["name"]
+		vs, err := s.FindVirtualServer(name)
+		if err != nil {
+			log.Errorf("FindVirtualServ err=%v", err)
+			WriteBadRequest(w, err)
+			return
+		}
+		server, err := decodeServer(r)
+		if err != nil {
+			WriteBadRequest(w, err)
+			return
+		}
+
+		vs.RemovePeer(server.Address)
+		io.WriteString(w, "Remove peer success")
 	})
 }
