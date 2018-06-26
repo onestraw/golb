96a5d184bb47c6aadda870df5d5d2d9b4c9d5955
onestraw <hexiaowei91@163.com>
2018-06-26 17:43:16 +0800

    Simplify the stats map operation

--- balancer/stats.go
+++ balancer/stats.go
@@ -21,13 +21,12 @@ func (s *Stats) Inc(addr string, code int) {
 	s.Lock()
 	defer s.Unlock()
 
-	if _, ok := s.StatusCode[addr]; !ok {
-		s.StatusCode[addr] = map[int]uint64{}
+	peer, ok := s.StatusCode[addr]
+	if !ok {
+		peer = map[int]uint64{}
+		s.StatusCode[addr] = peer
 	}
-	if _, ok := s.StatusCode[addr][code]; !ok {
-		s.StatusCode[addr][code] = 0
-	}
-	s.StatusCode[addr][code] += 1
+	peer[code] += 1
 }
 
 func (s *Stats) String() string {
@@ -35,9 +34,9 @@ func (s *Stats) String() string {
 	defer s.RUnlock()
 
 	result := []string{}
-	for addr, sc := range s.StatusCode {
+	for addr, peer := range s.StatusCode {
 		row := []string{addr}
-		for code, count := range sc {
+		for code, count := range peer {
 			row = append(row, fmt.Sprintf("%d:%d", code, count))
 		}
 		result = append(result, strings.Join(row, ", "))
