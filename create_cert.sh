d5df434149119f7d061c89d07e5b4b25a83cb62f
onestraw <hexiaowei91@163.com>
2018-07-04 17:58:40 +0800

    Add SSL example

---
+++ create_cert.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+# copyright: https://medium.com/@mlowicki/http-s-proxy-in-golang-in-less-than-100-lines-of-code-6a51c2f2c38c
+case `uname -s` in
+    Linux*)     sslConfig=/etc/ssl/openssl.cnf;;
+    Darwin*)    sslConfig=/System/Library/OpenSSL/openssl.cnf;;
+esac
+openssl req \
+    -newkey rsa:2048 \
+    -x509 \
+    -nodes \
+    -keyout server.key \
+    -new \
+    -out server.pem \
+    -subj /CN=localhost \
+    -reqexts SAN \
+    -extensions SAN \
+    -config <(cat $sslConfig \
+        <(printf '[SAN]\nsubjectAltName=DNS:localhost')) \
+    -sha256 \
+    -days 3650
